48c8c3e75549428d3ce3d378a8a50740e794dcf5
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-08-19 10:58:41 -0300

    Create Exemplo6.java
    Aula 19-08-25 
    Criação de Matrizes

---
+++ Exemplo6/Exemplo6.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Júlia
+ */
+public class Exemplo6 {
+    public static void main(String[] args) {
+    //Criação de Matriz
+    int TabNum[];
+    
+    TabNum = new int[3];
+    
+    TabNum[0] = 34;
+    TabNum[1] = 34;
+    TabNum[2] = 34;
+    
+    for(int cont=0;cont<3;cont++){
+        System.out.println("Conteudo do TabNum[" + cont + "] = " + TabNum[cont]);
+    }
+    
+    
+    }
+    
+}
