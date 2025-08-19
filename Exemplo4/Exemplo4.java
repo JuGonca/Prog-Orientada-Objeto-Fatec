413e29dc03f6b1f75c2ab02f6b413bc7cecfe0f4
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-08-19 10:29:59 -0300

    Create Exemplo4.java
    Aula 19-08-25
    Trocando o "for" do Exemplo 3 para o "while"

---
+++ Exemplo4/Exemplo4.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Júlia
+ */
+public class Exemplo4 {
+    public static void main(String[] args) {
+        int cont = 1;
+        
+        System.out.println("TABUADA DO 7");
+        while(cont<=10){
+           System.out.println("7 * " + cont + " = " + cont*7);
+           cont++;
+        }
+    }
+    
+}
