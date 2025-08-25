45128705c450fd0498066f56ba5cb702ee4a8777
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-08-25 09:15:48 -0300

    Update Aplic.java

--- Exemplos/ExemploRetangulo_1/Aplic.java
+++ Exemplos/ExemploRetangulo_1/Aplic.java
@@ -4,6 +4,8 @@
  */
 public class Aplic {
     public static void main(String[] args) {
+        System.out.println("RETANGULO 1");
+
         //definição do ponteiro
         Retangulo objRet;
         
@@ -25,10 +27,34 @@ public class Aplic {
         //Implementação do Caso de Uso
         //Calcular Perímetro
         System.out.println("Perímetro do Retângulo: " + objRet.calcPerimetro());
-
+        
         //Implementação do caso de uso
         //Calcular Diagonal Retangulo
         System.out.println("Diagonal: " + objRet.calcDiagonal());
+        
+        System.out.println("");
+        System.out.println("RETANGULO 2");
+        //Retangulo 2
+        Retangulo objRet2 = new Retangulo();
+        
+        objRet2.setAltura(3.0);
+        objRet2.setBase(4.0);
+        
+        System.out.println("Altura: " + objRet2.getAltura());
+        System.out.println("Base: " + objRet2.getBase());
+        
+        //Implementação do Caso de Uso
+        //Calcular Area
+        System.out.println("Área do Retangulo: " + objRet2.calcArea());
+        
+        //Implementação do Caso de Uso
+        //Calcular Perímetro
+        System.out.println("Perímetro do Retângulo: " + objRet2.calcPerimetro());
+        
+        //Implementação do caso de uso
+        //Calcular Diagonal Retangulo
+        System.out.println("Diagonal: " + objRet2.calcDiagonal());
+        
         
     }
     
