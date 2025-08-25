ecabef93f56ef87643393a8126bcd313b5fff44b
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-08-25 08:11:16 -0300

    Add files via upload

---
+++ Exemplos/Exemplo1.java
@@ -0,0 +1,28 @@
+package Exemplo1;
+
+/**
+ *
+ * @author Júlia Rocha
+ */
+public class Exemplo1 {
+
+    public static void main(String[] args) {
+        //Identificadores
+        int idade;
+        double peso;
+        char sexo;
+        String nome;
+        
+        idade = 34;
+        peso = 78.5;
+        sexo = 'F';
+        nome = "Ana Beatriz";
+        
+        //Comando para saida de dados
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Nome " + nome);
+    }
+    
+}
