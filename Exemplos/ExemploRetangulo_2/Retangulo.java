d9e2e72f64971db4f2dd2c04162cba70a9b776c4
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-08-26 10:28:20 -0300

    Create Retangulo.java
    Segundo exemplo

---
+++ Exemplos/ExemploRetangulo_2/Retangulo.java
@@ -0,0 +1,38 @@
+/**
+ *
+ * @author Júlia
+ */
+public class Retangulo {
+    private double altura;
+    private double base;
+    
+    //Adiciona valor
+    public void setAltura(double a){
+        altura = a;
+    }
+    
+    public void setBase(double b){
+        base = b;
+    }
+    
+    //Retorna Valor
+    public double getAltura(){
+    return altura;
+    }
+    
+    public double getBase(){
+    return base;
+    }
+    
+    public double calcArea(){
+        return(altura * base);
+    }
+    
+    public double calcPerimetro(){
+        return(2*(altura + base));
+    }
+    
+    public double calcDiagonal(){
+        return Math.sqrt(Math.pow(base, 2) + Math.pow(altura, 2));
+    }
+}
