6d7ddc636011d8d0ce54bbf49ec80a168974934c
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-09-03 09:33:28 -0300

    Update Aplic.java

--- Exercicios/Conta-Correnta/Aplic.java
+++ Exercicios/Conta-Correnta/Aplic.java
@@ -1,5 +1,5 @@
 /**
- * // Terminar atividade //
+ * 
  * @author Júlia
  */
 import java.util.Scanner;
@@ -8,16 +8,53 @@ public class Aplic {
 
     public static void main(String[] args) {
         int numeroConta;
-        double saldoAtual;
+        double saldoAtual = 0;
+        
+        int opcao = 0;
         
         Scanner entrada = new Scanner(System.in);
         
         System.out.println("Informe o número da conta: ");
         numeroConta = entrada.nextInt();
         
-        contaCorrente novaConta = new contaCorrente();
+        ContaCorrente novaConta = new ContaCorrente(numeroConta, saldoAtual);
         
-        novaConta.getNumero();
+        do{
+            System.out.println("\nDigite uma opção:");
+            System.out.println("1 - Depositar");
+            System.out.println("2 - Sacar");
+            System.out.println("3 - Consultar Saldo");
+            System.out.println("4 - Sair");
+            
+            opcao = entrada.nextInt();
+            
+            double valor;
+            if(opcao == 1){
+                System.out.println("\nConta: " + novaConta.getNumero());
+                System.out.println("Valor a depositar: R$");
+                valor = entrada.nextDouble();
+                novaConta.depositar(valor);
+            }
+            else if(opcao == 2){
+                System.out.println("\nConta: " + novaConta.getNumero());
+                System.out.println("Valor a sacar: R$");
+                valor = entrada.nextDouble();
+                if(novaConta.getSaldo() < valor){
+                    System.out.println("\nSaldo insuficiente.");
+                }
+                else{
+                    novaConta.sacar(valor);
+                    System.out.println("\nExecutado com sucesso.");
+                }
+            }
+            else if(opcao == 3){
+                System.out.println("\nConta: " + novaConta.getNumero());
+                System.out.println("Saldo Atual: R$"+novaConta.getSaldo());
+            }
+            else{
+                System.out.println("\nEncerrando...");
+            }
+        }while(opcao < 4);
     }
     
 }
