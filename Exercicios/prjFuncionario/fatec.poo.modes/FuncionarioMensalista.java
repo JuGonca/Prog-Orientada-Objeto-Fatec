eeb06cb2c44b19c4818a6bb82f27654c03a8e12a
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-09-16 10:24:13 -0300

    Create FuncionarioMensalista.java

---
+++ Exercicios/prjFuncionario/fatec.poo.modes/FuncionarioMensalista.java
@@ -0,0 +1,20 @@
+package fatec.poo.model;
+/**
+ *
+ * @author Júlia
+ */
+public class FuncionarioMensalista extends Funcionario{
+    private double valSalMin;
+    private double numSalMin;
+    
+    public FuncionarioMensalista(){int r, String n String dt, double sm}{
+        super(r, n, dt);
+        valSalMin = sm;
+    }
+    
+    public void setNumSalMin(double nm){
+        numSalMin = nm;
+    }
+    
+    public double salBruto();
+}
