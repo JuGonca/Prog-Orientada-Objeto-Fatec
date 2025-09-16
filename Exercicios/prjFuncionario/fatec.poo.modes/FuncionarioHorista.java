f2184d43c1815174460e49c258531c55bae271db
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-09-16 10:23:36 -0300

    Create FuncionarioHorista.java

---
+++ Exercicios/prjFuncionario/fatec.poo.modes/FuncionarioHorista.java
@@ -0,0 +1,22 @@
+package fatec.poo.model;
+/**
+ *
+ * @author Júlia
+ */
+public class FunicionarioHorista extends Funcionario{
+    private double valorHoraTrab;
+    private int qtdHoraTrab;
+    
+    public FuncionarioHorista(int r, String n String dt, double vh){
+        super(r,n,dt);
+        valorHoraTrab = vh;
+    }
+    
+    public void setQtdHoriario(int qht){
+        qtdHoraTrab = qht;
+    }
+    
+    public double calcSalBruto(){
+        return(valorHoraTrab * qtdHoraTrab);
+    }
+}
