00d5f528816e6a4b421c9e8c04575e7880171693
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-10-14 11:04:23 -0300

    Update Departamento.java
    Definindo métodos addfuncionario e listaFuncionario

--- Exemplos/prjExemploAssociacaoBinaria/fatec.poo.model/Departamento.java
+++ Exemplos/prjExemploAssociacaoBinaria/fatec.poo.model/Departamento.java
@@ -24,11 +24,19 @@ public class Departamento {
         return nome;
     }
     
-    public void addFuncionario(funcionarios){
-        
+    public void addFuncionario(Funcionario f){
+        funcionarios[numFunc++] = f;
     }
     
-    public void listaFuncionario(funcionarios){
-    
+    public void listaFuncionario(){
+        System.out.println("\n\nSigla: " + sigla);
+        System.out.println("Nome: " + nome);
+        System.out.println("Qtd Funcionarios: " + numFunc);
+        System.out.println("\nRegistro\t\tNome\t\tCargo");
+        for(int x=0; x < numFunc; x++){
+            System.out.print(funcionarios[x].getRegistro());
+            System.out.print(funcionarios[x].getNome());
+            System.out.print(funcionarios[x].getCargo());
+        }
     }
 }
